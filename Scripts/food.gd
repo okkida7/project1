50a646b999a0cb267406db5010d120d0bc481173
Cody Jiang <93929791+Cody-Jiang-Zhihong@users.noreply.github.com>
2023-09-17 19:13:33 -0400

    Update food.gd

--- Scripts/food.gd
+++ Scripts/food.gd
@@ -39,7 +39,48 @@ func calculate_intersection(from, to):
 	else:
 		return null
 
+# Cody's newest update:
+
+func crop_image(original_image: Image, crop_rect: Rect2) -> Image:
+    var cropped = Image.new()
+    cropped.create(crop_rect.size.x, crop_rect.size.y, false, original_image.get_format())
+    cropped.blit_rect(original_image, crop_rect, Vector2(0, 0))
+    return cropped
+
+func generate_half_sprite(cropped_image: Image, position: Vector2):
+    var texture = ImageTexture.new()
+    texture.create_from_image(cropped_image)
+    var new_sprite = Sprite.new()
+    new_sprite.texture = texture
+    new_sprite.position = position
+    return new_sprite
+
 func split_sprite(intersections):
-	print("Slicing sprite at: ", intersections)
-	# we need a sprite slicing logic here
-	queue_free()  # For this example, we'll just delete the sprite
+    var original_image = $Sprite.texture.get_data()
+    original_image.lock()
+
+    # Calculate the crop rectangles based on the slice line and sprite dimensions.
+    # This logic will vary based on how exactly you're slicing the sprite.
+    var rect1 = Rect2(0, 0, intersections[0].x, original_image.get_height())
+    var rect2 = Rect2(intersections[1].x, 0, original_image.get_width() - intersections[1].x, original_image.get_height())
+    
+    var cropped_image1 = crop_image(original_image, rect1)
+    var cropped_image2 = crop_image(original_image, rect2)
+
+    # Generate new sprites and set their positions relative to the original sprite.
+    var new_sprite1 = generate_half_sprite(cropped_image1, Vector2(rect1.position.x, rect1.position.y))
+    var new_sprite2 = generate_half_sprite(cropped_image2, Vector2(rect2.position.x, rect2.position.y))
+
+    # Add these new sprites to new RigidBody2D nodes (or other types depending on your game logic)
+    var body1 = RigidBody2D.new()
+    body1.add_child(new_sprite1)
+    body1.global_position = self.global_position + rect1.position
+
+    var body2 = RigidBody2D.new()
+    body2.add_child(new_sprite2)
+    body2.global_position = self.global_position + rect2.position
+
+    # Add new bodies to the scene and remove the original one.
+    get_parent().add_child(body1)
+    get_parent().add_child(body2)
+    queue_free()
