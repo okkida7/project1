4f63bfcd4e9088c68fd5d1ece0e75df164ab76bc
Alexis Jin <xj606@nyu.edu>
2023-09-17 20:55:21 -0400

    Scenes update

--- Scripts/start_menu.gd
+++ Scripts/start_menu.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/main_game.tscn")
 
 
 func _on_quit_button_pressed():
